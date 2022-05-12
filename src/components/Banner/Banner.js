3b6c59cc1c44c7cd3cee0fafed74a53825b66dd9
rupok <nurealamsiddiky@gmail.com>
2022-05-12 07:59:51 +0600

    banner section is edited and text animation is now starts on banner image load

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
 
 const Banner = () => {
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        document.getElementById("banner-img").onload = (event) => {
+            setLoaded(true);
+        };
+    }, []);
+
     return (
         <div>
             <div className="mx-auto p-4 lg:px-16 md:px-8">
                 <div className="bg-gray-900 rounded-2xl relative h-auto">
                     <img
-                        className="mx-auto w-full rounded-2xl opacity-70"
+                        id="banner-img"
+                        className=" mx-auto w-full rounded-2xl opacity-70"
                         src="images/fruitox-banner.jpg
                         "
                         alt="Banner contains fresh fruits"
                     />
-                    <h1 className="description absolute lg:top-[70%] md:top-[60%] top-[50%] md:left-[12%] left-[10%] w-4/5  md:text-5xl text-3xl text-white drop-shadow-md font-bold font-cabin">
+                    <h1
+                        className={`${
+                            loaded ? "description" : ""
+                        } absolute lg:top-[70%] md:top-[60%] top-[50%] md:left-[12%] left-[10%] w-4/5  md:text-5xl text-3xl text-white drop-shadow-md font-bold font-cabin`}
+                    >
                         Largest Fruit Warehouse In The City
                     </h1>
                 </div>
